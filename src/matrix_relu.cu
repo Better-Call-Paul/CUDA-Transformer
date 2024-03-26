f939d2d90aa83e3491d2aafd06e800027cba0cc0
Ubuntu <ubuntu@ip-172-31-92-20.ec2.internal>
2024-03-26 04:40:56 +0000

    Added Tensor Multiplication

---
+++ src/matrix_relu.cu
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ctime>
+#include <cassert>
+#include <cstdlib>
+
+__global__ void relu(float *a, float *b, float *c) {
+    
+}
+
+
+int main(int argc, char *argv[]) {
+
+
+
+
+    return 0;
+}
