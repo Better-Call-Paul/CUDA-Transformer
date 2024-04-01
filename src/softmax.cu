831e197576a1fbd746b3d39af733fbde5e1ea498
Ubuntu <ubuntu@ip-172-31-30-70.ec2.internal>
2024-04-01 22:39:16 +0000

    adding softmax

--- src/softmax.cu
+++ src/softmax.cu
@@ -5,83 +5,109 @@
 #include <cmath>
 #include <curand.h>
 
+#define SHMEM_SIZE 256 
 
-#define SHMEM_SIZE 8 * 8 * 8
-
-
-__device__ void warp_reduce(volative float *shmeme_ptr, int thread_id) {
-
+__inline__ __device__ 
+float warp_reduce(volatile float* shmem_ptr, int thread_id) {
+    if (thread_id < 32) {
+        shmem_ptr[thread_id] += (thread_id + 32 < blockDim.x) ? shmem_ptr[thread_id + 32] : 0;
+        shmem_ptr[thread_id] += (thread_id + 16 < blockDim.x) ? shmem_ptr[thread_id + 16] : 0;
+        shmem_ptr[thread_id] += (thread_id + 8 < blockDim.x) ? shmem_ptr[thread_id + 8] : 0;
+        shmem_ptr[thread_id] += (thread_id + 4 < blockDim.x) ? shmem_ptr[thread_id + 4] : 0;
+        shmem_ptr[thread_id] += (thread_id + 2 < blockDim.x) ? shmem_ptr[thread_id + 2] : 0;
+        shmem_ptr[thread_id] += (thread_id + 1 < blockDim.x) ? shmem_ptr[thread_id + 1] : 0;
+    }
+    return shmem_ptr[thread_id];
 }
 
-__global__ void sum_reduction(float *input, float *output) {
+__global__ void softmax(float *input, float *output, int N, int M) {
+    
+    extern __shared__ float shared_mem[];
+    int thread_index = threadIdx.x;
+    int global_index = blockIdx.x * blockDim.x + threadIdx.x;
 
-    __shared__ float partial_sum[SHMEM_SIZE];
-
-    // calculate indexs
-
-    // load elements and do first add reduction
-    // vectir biw 2x as long as num of threads, scale i
-
-    // store first partial result instead of just the elements
-    for (int k = blockDix.x / 2; k > 32; k >>= 1) {
-
-        if (threadIdx.x < k) {
-            partial
-        }
-        __syncthreads();
+    // Only compute exponentials for valid (non-padded) values 
+    if (thread_index < N * M) {
+        shared_mem[thread_index] = exp(shared_mem[thread_index]);
+    }
+    else {
+        shared_mem[thread_index] = 0.0f;
     }
 
-    if (threadIdx.x < 32) {
-        warp_reduce(partial_sum, threadIdx.x);
-    }
+    // Ensure all exponentials are computed
+    __syncthreads();
 
+    // warp_reduce
+
+    // get the value in the 0th index 
     if (threadIdx.x == 0) {
-        output[blockIdx.x] = partial_sum[0];
+        
     }
 }
 
-__global__ void softmax(float* input, float *exponents, float *) {
-
+void check_softmax(float *input, float *output, float *computed_output, int N, int M, const float tolerance) {
+    for (int i = 0; i < N; i++) {
+        float sum = 0.0f;
+        for (int j = 0; j < M; j++) {
+            computed_output[i * M + j] = exp(input[i * M + j]);
+            sum += computed_output[i * M + j];
+        }
+        for (int k = 0; k < M; k++) {
+            computed_output[i * M + k] /= sum;
+            assert(std::fabs(computed_output[i * M + k] - output[i * M + k]) < tolerance);
+        }
+    }
 }
 
-int main(int argc, char *argv[]) {
+void handle_cuda_error(cudaError_t error, const char* message) {
+    if (error != cudaSuccess) {
+        std::cerr << message << ": " << cudaGetErrorString(error) << "\n";
+        exit(-1);
+    }
+}
 
+int main() {
+    const float tolerance = 1e-3;
     int number = 1 << 8;
-    int N = number;
-    size_t size = N * N * sizeof(float);
-    const float epsilon = 1e-3;
+    int N = number, M = number;
+    size_t size = N * M * sizeof(float);
 
-    float *input_vector, *output_vector;
-    float *exponent_holder, reduction_holder;
-    float sum = 0.0f;
+    float *host_input_vector, *host_output_vector, *device_input_vector, *device_output_vector, *iterative_output_vector;
+
+    host_input_vector = (float*)malloc(size);
+    host_output_vector = (float*)malloc(size);
+    iterative_output_vector = (float*)malloc(size);
     
-    cudaMalloc(input_vector, size);
-    cudaMalloc(output_vector, size);
-    cudaMalloc(exponent_holder, size);
-    cudaMalloc(reduction_holder, size);
-
-
+    // Populate host vectors
     curandGenerator_t generator;
-    curandCreateGenerator(&generator, CURAND_RNG_PSEUDO_DEFAULT);
+    curandCreateGeneratorHost(&generator, CURAND_RNG_PSEUDO_DEFAULT);
+    curandSetPseudoRandomGeneratorSeed(generator, (unsigned long long)clock());
+    curandGenerateUniform(generator, host_input_vector, N * M);
 
-    curandGenerateUniform(generator, srand(time(NULL)));
+    cudaMalloc(&device_input_vector, size);
+    cudaMalloc(&device_output_vector, size);
 
+    cudaMemcpy(device_input_vector, host_input_vector, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(device_output_vector, host_output_vector, size, cudaMemcpyHostToDevice);
+
+
+    dim3 block_dimensions(8, 8);
+
+    dim3 threads(
+        (N + block_dimensions.x - 1) / block_dimensions.x,
+        (M + block_dimensions.y - 1) / block_dimensions.y
+    );
+
+    softmax<<<block_quantity, threads, SHMEM_SIZE * sizeof(float)>>>(device_input_vector, device_output_vector, N, M);
     
+    handle_cuda_error(cudaGetLastError(), "Kernel Failed");
 
+    cudaMemcpy(host_output_vector, device_output_vector, size, cudaMemcpyDeviceToHost);
 
+    check_softmax(host_input_vector, host_output_vector, iterative_output_vector, N, M, tolerance);
 
-
-
-
-
-
-
-
-
-    
-
-
-
+    cudaFree(device_input_vector);
+    cudaFree(device_output_vector);
 
     return 0;
 }
